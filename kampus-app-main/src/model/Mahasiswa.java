6fd4ab5c6df5dae31eaf8bdd54a8794f9e6c024d
Rafdy <102248201+dfrayyy@users.noreply.github.com>
2023-11-04 09:14:37 +0700

    Add files via upload

---
+++ kampus-app-main/src/model/Mahasiswa.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Mahasiswa {
+    private String nim;
+    public String getNim() {
+        return nim;
+    }
+
+    private String nama;
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    private String idProdi;
+
+    public String idProdi(){
+        return idProdi;
+
+    }
+    
+
+
+    public Mahasiswa(String nim, String nama, String idProdi) {
+        this.nim = nim;
+        this.nama = nama;
+        this.idProdi = idProdi;
+    }
+
+    public Object getIdProdi() {
+        return null;
+    }
+}
